a57f73d3bbb1cf5fd4b4a905dcee1d0af664ceda
Christopher Shin <uqwe456@gmail.com>
2024-01-23 19:43:43 +0900

    Practice_1

---
+++ Practice_1.R
@@ -0,0 +1,15 @@
+# install.packages('usethis')
+library(usethis)
+
+## Name, email
+usethis::use_git_config(
+  user.name = "신상원"
+  user.email = "uqwe456@gmail.com"
+)
+
+## Setting: permanent storage
+credentials::credential_helper_set("manager")
+
+
+## create a personal access token for authentication
+usethis::create_github_token()
